c0ab9bd1c20bfd899653e7037cc92e506cbba7c4
MadMarvi <111076792+MadMarvi@users.noreply.github.com>
2025-08-16 03:42:17 +0200

    Update rounding_module.v

--- Real_Mul/rounding_module.v
+++ Real_Mul/rounding_module.v
@@ -1,30 +1,29 @@
 module rounding_module #(
     parameter IS_DOUBLE = 0
-) (
-    input wire clk,            
-    input wire rst,         
+) (         
     input wire [((IS_DOUBLE) ? 105 : 47):0] data_in,  
-    input wire [1:0] round_mode,
-                                  // 00 - Округление к нулю
+    input wire [1:0] round_mode,  // 00 - Округление к нулю
                                   // 01 - Округление к + inf
                                   // 10 - Округление к - inf
                                   // 11 - Округление к ближайшему четному
-    output reg [((IS_DOUBLE) ? 52 : 23):0] data_out, 
-    output reg acc            // Флаг точности (1 - округление не потребовалось)
+    output wire [((IS_DOUBLE) ? 52 : 23):0] data_out, 
+    output wire inexact              // Флаг точности (1 - округление не потребовалось)
 );
 
-wire [((IS_DOUBLE) ? 52 : 23):0] high_part = data_in[((IS_DOUBLE) ? 105 : 47):((IS_DOUBLE) ? 53 : 24)];
-wire [((IS_DOUBLE) ? 52 : 23):0] low_part  = data_in[((IS_DOUBLE) ? 52 : 23):0];
+localparam HIGH_PART_WIDTH = (IS_DOUBLE) ? 52 : 23;
+localparam LOW_PART_WIDTH = (IS_DOUBLE) ? 53 : 24;
+localparam TOTAL_WIDTH = (IS_DOUBLE) ? 106 : 48;
 
+wire [HIGH_PART_WIDTH:0] high_part = data_in[TOTAL_WIDTH-1:LOW_PART_WIDTH];
+wire [LOW_PART_WIDTH-1:0] low_part = data_in[LOW_PART_WIDTH-1:0];
 
 wire low_part_is_zero = (low_part == 0); // флаг на то что округления не было
-wire sign_bit = high_part[((IS_DOUBLE) ? 52 : 23)]; // Знаковый бит
+wire sign_bit = high_part[HIGH_PART_WIDTH]; // Знаковый бит
 
 // биты для окургления к четному
 wire round_bit  = high_part[0];
-wire guard_bit  = low_part[((IS_DOUBLE) ? 52 : 23)];
-wire sticky_bit = (IS_DOUBLE) ? (|low_part[51:0]) : (|low_part[22:0]);
-
+wire guard_bit  = low_part[LOW_PART_WIDTH-1];
+wire sticky_bit = (IS_DOUBLE) ? (|low_part[LOW_PART_WIDTH-2:0]) : (|low_part[22:0]);
 
 // Вычисление инкремента для каждого режима округления
 wire round_up_plus_inf  = ~sign_bit & ~low_part_is_zero; // Для + inf если число положительное
@@ -41,16 +40,7 @@ wire increment =
     1'b0;
 
 // Финальное округленное значение
-wire [((IS_DOUBLE) ? 52 : 23):0] rounded_data = (low_part_is_zero ? high_part : high_part + increment);
- 
-always @(posedge clk) begin
-    if (rst) begin
-        data_out <= 0;
-        acc <= 1'b0;
-    end else begin
-        data_out <= rounded_data;
-        acc <= low_part_is_zero;
-    end
-end
+assign data_out = high_part + increment;
+assign inexact = ~low_part_is_zero;
 
 endmodule
