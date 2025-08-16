f5821c567e9dd60b37f36c6c398fed7f0c20d6f5
MadMarvi <111076792+MadMarvi@users.noreply.github.com>
2025-08-16 04:10:08 +0200

    Update Zero_Counter.v

--- Zero_Counter.v
+++ Zero_Counter.v
@@ -1,38 +1,38 @@
-module Z_C #(
+module Zero_Counter #(
     parameter DATA_WIDTH = 16,
-    parameter ZERO_WIDTH = $clog2(DATA_WIDTH+1) // Разрядность суммы(+1 так как нулей может быть 16)
-    parameter LEFT_CNT = 0
+    parameter ZERO_WIDTH = $clog2(DATA_WIDTH+1),
+    parameter LEFT_CNT = 1                  // 1 - считать старшие нули (слева), 0 - младшие (справа)
 )(
-    input wire [DATA_WIDTH-1:0] data, // входные данные
-    output wire [ZERO_WIDTH-1:0] zero_num // выходные данные
+    input wire [DATA_WIDTH-1:0] data,
+    output wire [ZERO_WIDTH-1:0] zero_num
 );
 
-wire [ZERO_WIDTH-1:0] nmb [0:DATA_WIDTH]; //Хранение количества 0
-assign nmb[DATA_WIDTH] = 0; //Начальное значение для сравнения в цикле
-wire flag [0:DATA_WIDTH]; //Указатель на нахождение 1 после старших 0
-assign flag[DATA_WIDTH] = 0; // 1 элемент равен 0(изначально ноль нулей)
-wire [DATA_WIDTH-1:0] data_1;
-genvar i;
+wire [DATA_WIDTH-1:0] data_reversed;
+wire [ZERO_WIDTH-1:0] nmb [0:DATA_WIDTH];   //количество нулей
+wire flag [0:DATA_WIDTH];                   //указатель на нахождение первой единицы
 
-generate if (LEFT_CNT==1) // Вопрос к модулю, успеет ли перевернуться входные данные, до срабатывания подсчета нулей
-begin: cnt_high_bits
-    assign data_1 = data;
-end
-else begin: cnt_low_bits
-    for (i = 0; i<=DATA_WIDTH; i = i + 1) begin:
-    assign data_1 = data[DATA_WIDTH-1-i];
+// Разворот данных, если нужно считать младшие нули
+generate
+    genvar i;
+    for (i = 0; i < DATA_WIDTH; i = i + 1) begin: reverse_data
+        assign data_reversed[i] = data[DATA_WIDTH-1-i];
     end
-end
 endgenerate
 
+wire [DATA_WIDTH-1:0] data_processed = (LEFT_CNT) ? data : data_reversed;
 
+assign flag[DATA_WIDTH] = 0;
+assign nmb[DATA_WIDTH] = 0;
+
+// Подсчет нулей
 generate
-    for (i = DATA_WIDTH-1; i >= 0; i = i - 1) begin: ZERO_SUM //Идем с левого конца
-        assign flag[i] = flag[i+1] || data[i]; // поиск 1, если была найдена 1, то далее флаг будет выдавать 1 каждую итерацию, иначе 0(Флаг в обратную сторону, nmb слева направо)
-        assign nmb[i] = (flag[i+1] || data[i]) ? nmb[i+1] : (nmb[i+1] + 1); // Если был флаг, то в ячейку записывает предыдущее, если нет, к предыдущему значению в ячейку +1, после нахождения первой единицы флаг всегда 1 и следоватенльно всегда записывется максимальное найденное число 0   
-        end  
+    for (i = DATA_WIDTH-1; i >= 0; i = i - 1) begin: count_zeros
+        assign flag[i] = flag[i+1] || data_processed[i];
+        assign nmb[i] = (flag[i+1] || data_processed[i]) ? nmb[i+1] : (nmb[i+1] + 1);
+    end
 endgenerate
 
 assign zero_num = nmb[0];
 
 endmodule
+
