2d75dc613699c1419b8e71ae529780c735ddb85b
MadMarvi <111076792+MadMarvi@users.noreply.github.com>
2025-08-16 14:54:19 +0200

    Update Rounding_module.v

--- Real_Mul/Rounding_module.v
+++ Real_Mul/Rounding_module.v
@@ -1,33 +1,33 @@
 module rounding_module #(
-    parameter IS_DOUBLE = 0
+    parameter IS_DOUBLE = 0,
+    parameter HIGH_PART_WIDTH = (IS_DOUBLE) ? 52 : 23,
+    parameter LOW_PART_WIDTH = (IS_DOUBLE) ? 53 : 24,
+    parameter TOTAL_WIDTH = (IS_DOUBLE) ? 106 : 48
 ) (         
-    input wire [((IS_DOUBLE) ? 105 : 47):0] data_in,  
+    input wire [TOTAL_WIDTH-1:0] data_in,  
     input wire [1:0] round_mode,  // 00 - Округление к нулю
                                   // 01 - Округление к + inf
                                   // 10 - Округление к - inf
                                   // 11 - Округление к ближайшему четному
-    input  wire res_sign
-    output wire [((IS_DOUBLE) ? 52 : 23):0] data_out, 
-    output wire inexact              // Флаг точности (1 - округление не потребовалось)
+    input wire res_sign,
+    output wire [HIGH_PART_WIDTH:0] data_out, 
+    output wire inexact,           // Флаг точности (1 - округление не потребовалось)
+    output wire overflow           // Флаг переполнения
 );
-
-localparam HIGH_PART_WIDTH = (IS_DOUBLE) ? 52 : 23;
-localparam LOW_PART_WIDTH = (IS_DOUBLE) ? 53 : 24;
-localparam TOTAL_WIDTH = (IS_DOUBLE) ? 106 : 48;
 
 wire [HIGH_PART_WIDTH:0] high_part = data_in[TOTAL_WIDTH-1:LOW_PART_WIDTH];
 wire [LOW_PART_WIDTH-1:0] low_part = data_in[LOW_PART_WIDTH-1:0];
 
-wire low_part_is_zero = (low_part == 0); // флаг на то что округления не было
+wire low_part_is_zero = (low_part == 0); // Флаг на то, что округления не было
 
-// биты для окургления к четному
+// Биты для округления к четному
 wire round_bit  = high_part[0];
 wire guard_bit  = low_part[LOW_PART_WIDTH-1];
 wire sticky_bit = (IS_DOUBLE) ? (|low_part[LOW_PART_WIDTH-2:0]) : (|low_part[22:0]);
 
 // Вычисление инкремента для каждого режима округления
-wire round_up_plus_inf  = ~res_sign & ~low_part_is_zero; // Для + inf если число положительное
-wire round_up_min_inf   =  res_sign & ~low_part_is_zero; // Для - inf если число отрицательное
+wire round_up_plus_inf  = ~res_sign & ~low_part_is_zero; // Для +inf если число положительное
+wire round_up_min_inf   =  res_sign & ~low_part_is_zero; // Для -inf если число отрицательное
 
 wire round_nearest_even = ((guard_bit & sticky_bit) | 
                          (guard_bit & ~sticky_bit & round_bit));
@@ -39,8 +39,12 @@ wire increment =
     (round_mode == 2'b11) ? round_nearest_even :
     1'b0;
 
-// Финальное округленное значение
-assign data_out = high_part + increment;
+// Проверка на переполнение (все биты high_part равны 1 и есть инкремент)
+wire high_part_all_ones = &high_part;
+assign overflow = high_part_all_ones & increment;
+
+// Финальное округленное значение (при переполнении: 1 с нулями)
+assign data_out = overflow ? {1'b1, {HIGH_PART_WIDTH{1'b0}}} : (high_part + increment);
 assign inexact = ~low_part_is_zero;
 
 endmodule
