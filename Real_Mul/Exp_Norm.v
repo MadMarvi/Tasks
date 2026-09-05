34b2c4f589f5aa8d52627db4fba92fec5102dc54
MadMarvi <111076792+MadMarvi@users.noreply.github.com>
2026-09-05 18:45:25 +0300

    Update Exp_Norm.v

--- Real_Mul/Exp_Norm.v
+++ Real_Mul/Exp_Norm.v
@@ -1,13 +1,15 @@
 module Exp_Normalizer #(
     parameter IS_DOUBLE = 0,
     parameter EXP_WIDTH = IS_DOUBLE ? 11 : 8,
-    parameter MANT_WIDTH = IS_DOUBLE ? 53 : 24;
+    parameter MANT_WIDTH = IS_DOUBLE ? 53 : 24,
     parameter EXP_BIAS = IS_DOUBLE ? 1023 : 127
 ) (
-    input wire is_normalized1,
-    input wire is_normalized2,
-    input wire [EXP_WIDTH+1:0] exp_in,
-    output wire [EXP_WIDTH-1:0] exp_out,
+    input  wire                   is_normalized1,
+    input  wire                   is_normalized2,
+    input  wire [EXP_WIDTH+1:0]   exp_in,
+    output wire [EXP_WIDTH-1:0]   exp_out
 );
-   
+
+    assign exp_out = exp_in[EXP_WIDTH-1:0];
+
 endmodule
