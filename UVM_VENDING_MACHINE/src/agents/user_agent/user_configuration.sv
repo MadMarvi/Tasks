394c4265355c340d2500d537729d11d2c63d9701
MadMarvi <111076792+MadMarvi@users.noreply.github.com>
2026-09-05 12:24:03 +0300

    Create user_configuration.sv

---
+++ UVM_VENDING_MACHINE/src/agents/user_agent/user_configuration.sv
@@ -0,0 +1,8 @@
+class user_configuration extends uvm_object;
+   `uvm_object_utils(user_configuration)
+
+
+   function new(string name = "user_configuration");
+      super.new(name);
+   endfunction: new
+endclass: user_configuration
